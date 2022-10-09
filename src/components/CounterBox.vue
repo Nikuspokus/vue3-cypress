705443306f77f1c07aa77d6e6abf67ceada632cf
nmarquillier <nmarquillier@norauto.com>
2022-10-09 17:10:14 +0200

    first commiit nico

---
+++ src/components/CounterBox.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <div style="justify-content: center">
+      <span>{{ multiplierCount }}</span>
+    </div>
+    <button @click="increment">Increments</button
+    ><button @click="decrement">Decrements</button
+    ><button @click="reset">Reset</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    multiplier: { type: Number, default: 1 },
+  },
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+    decrement() {
+      this.count--;
+    },
+    reset() {
+      this.count = 0;
+    },
+  },
+  computed: {
+    multiplierCount: function () {
+      return this.count + this.multiplier;
+    },
+  },
+};
+</script>
+
+<style>
+div {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+  margin-bottom: 1rem;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>
+    <div style="justify-content: center">
+      <CounterBox :multiplier="0" />
+    </div>
+  </main>
+</template>
+
+<script setup>
+import CounterBox from "../components/CounterBox.vue";
+</script>
+
+<style>
+div {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+</style>
